940f7f81315c6fabaf407be7f52a2d7c29dce5ae
Geert Fannes <gfannes@ri.(none)>
2009-07-30 08:20:54 +0200

    Minor modifications

--- d/gubg/sequence.d
+++ d/gubg/sequence.d
@@ -1,6 +1,7 @@
 module gubg.sequence;
 
 import gubg.math;
+import gubg.vector;
 import gubg.puts;
 
 import tango.math.Math;
@@ -39,14 +40,24 @@ class Sequence
     real[] targetValues()
 	{
 	    real[] res = new real[nrStates];
+	    
 	    foreach (ix, inout v; res)
 	    {
 		res[ix] = _stay*_probs[ix];
 		if (ix == 0)
-		    res[ix] += _reset;
+		    res[ix] += _reset + _move*_probs[$-1];
 		else
 		    res[ix] += _move*_probs[ix-1];
 	    }
+
+	    auto maxIX = maxIndex(_probs);
+	    puts("\nmaxIX = {}", maxIX);
+	    puts("_probs = {}", _probs);
+	    puts("targetValues before extra = {}", res);
+	    res[(maxIX+1)%$] += 0.8*_move*_probs[maxIX];
+//	    res[maxIX] -= 0.8*_stay*_probs[maxIX];
+
+	    puts("targetValues before shift = {}", res);
 	    res.shiftMeanTo(0.0);
 	    return res;
 	}
@@ -61,13 +72,13 @@ private:
     real[] _currentValues;
 }
 
-version (UnitTest)
+version (UnitTest2)
 {
     import gubg.timer;
     import tango.core.Thread;
     void main()
     {
-        real stay = 1.0, move = 0.2, reset = 2;
+        real stay = 1.0, move = 0.2, reset = 0.1;
         real[] probs = [0.1, 0.2, 0.3];
 	auto seq = new Sequence(stay, move, reset, probs);
 	puts("probs = {}, sum = {}", seq.probs, sum(seq.probs));
@@ -77,8 +88,66 @@ version (UnitTest)
         {
             seq.update(timer.difference);
             auto p = seq.probs;
-            puts("probs = {:g9}, {:g9}, {:g9}, sum = {}", p[0], p[1], p[2], sum(seq.probs));
-            Thread.sleep(0.1);
+//            puts("probs = {:g9}, {:g9}, {:g9}, sum = {}", p[0], p[1], p[2], sum(seq.probs));
+//            Thread.sleep(0.1);
         }
+    }
+}
+
+version (UnitTest)
+{
+    import gubg.timer;
+    import tango.core.Thread;
+
+    class NewtonSpring
+    {
+	this(real springK)
+	    {
+		_springK = springK;
+		_pos = _speed = _accel = 0.0;
+		_externalAccel = 0.0;
+	    }
+
+	void setAccel(real accel){_externalAccel = accel;}
+	void setSpeed(real speed){_speed = speed;}
+
+	void update(real dT)
+	    {
+		_accel = _externalAccel + springAccel;
+		_speed += dT*_accel;
+		_pos += dT*_speed;
+	    }
+
+	real position(){return _pos;}
+	real speed(){return _speed;}
+
+    private:
+	real springAccel(){return -_springK*_pos;}
+
+	// Fixed
+	real _springK;
+
+	// External
+	real _externalAccel;
+
+	// Volatile
+	real _pos;
+	real _speed;
+	real _accel;
+    }
+
+    void main()
+    {
+	auto timer = new Timer;
+	auto m = new NewtonSpring(1);
+
+	m.setSpeed(1.0);
+	
+	for (uint i = 0; i < 1000; ++i)
+	{
+	    Thread.sleep(0.1);
+	    m.update(timer.difference);
+	    puts("pos = {}", m.position);
+	}
     }
 }
--- d/gubg/vector.d
+++ d/gubg/vector.d
@@ -10,6 +10,19 @@ real inprod(real[] lhs, real[] rhs)
     return inp;
 }
 
+uint maxIndex(real[] values)
+{
+    uint res;
+    real maxValue;
+    foreach (ix, v; values)
+	if (ix == 0 || v > maxValue)
+	{
+	    res = ix;
+	    maxValue = v;
+	}
+    return res;
+}
+
 version (UnitTest)
 {
     import gubg.puts;
