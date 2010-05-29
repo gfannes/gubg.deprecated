b06eeaae00177a00bdcf334d966a3cbdd74d55a4
Geert Fannes <geert.fannes@gmail.com>
2010-05-29 11:45:25 +0200

    Implemented iteration over a tree

--- ruby/gb/tree.rb
+++ ruby/gb/tree.rb
@@ -1,41 +1,67 @@
 #A Tree is a collection of files defined by a specific anchorPoint in a file system:
-# * Step from anchorPoint towards / until "root.tree" is found
+# * Step from anchorPoint towards "/" until "root.tree" is found
 # * All files in all subdirectories from "root.tree" are part of the collection
 # * "root.tree: can include other files collections or exclude other files
 
 require("root")
+require("gubg/utils")
 
 class Tree
     @@rootFileName = "root.tree"
 
     def initialize(anchorPoint)
-        @anchorPoint = File.expand_path(anchorPoint)
+        @anchorPoint = ::File.expand_path(anchorPoint)
         loadRoot_
-        createLocalFileList_
+        createLocalFileCollection_
         loadChildren_
     end
 
     def each(&block)
+        @filesPerExtension.each do |extension, files|
+            files.each{|file|block.call(file)}
+        end
     end
 
     private
     def loadRoot_
-        #Find @rootFile: step down from @anchorPoint towards /, until "root.tree" is found
-        @rootFile = nil
-        directory = File.directory?(@anchorPoint) ? @anchorPoint : File.dirname(@anchorPoint)
+        #Find @rootFilePath: step down from @anchorPoint towards "/", until "root.tree" is found
+        @rootFilePath = nil
+        @rootDirectory = nil
+        directory = ::File.directory?(@anchorPoint) ? @anchorPoint : ::File.dirname(@anchorPoint)
         loop do
-            tmpRootFile = File.expand_path(@@rootFileName, directory)
-            if File.exist?(tmpRootFile)
-                @rootFile = tmpRootFile
+            tmpRootFile = ::File.expand_path(@@rootFileName, directory)
+            if ::File.exist?(tmpRootFile)
+                @rootFilePath = tmpRootFile
+                @rootDirectory = directory
                 break
             end
-            nextDirectory = File.dirname
+            nextDirectory = ::File.dirname
             raise("No #{@@rootFile} found") if nextDirectory == directory
         end
         #Load "root.tree"
-        @root = Root.createFromFile(@rootFile)
+        puts("rootFilePath = #{@rootFilePath}")
+        @root = Root.createFromFile(@rootFilePath)
     end
-    def createLocalFileList_
+    class File
+        attr(:directory, true)
+        attr(:filename, true)
+        def initialize(directory, fileName)
+            @directory, @fileName = directory, fileName
+            @extension = ::File.extname(@fileName)
+        end
+        def extension
+            @extension
+        end
+        def to_s
+            ::File.expand_path(@fileName, @directory)
+        end
+    end
+    def createLocalFileCollection_
+        @filesPerExtension = Hash.new{|h, k|h[k] = []}
+        Dir.each(@rootDirectory) do |dir, fn|
+            File.new(dir, fn).tap{|file|@filesPerExtension[file.extension] << file}
+            nil
+        end
     end
     def loadChildren_
     end
@@ -43,7 +69,7 @@ end
 
 if __FILE__ == $0
     tree = Tree.new("/home/gfannes/gubg/d/gubg/Sequence.d")
-    tree.each do |fileName|
-        puts(fileName)
+    tree.each do |file|
+        puts("#{file}")
     end
 end
