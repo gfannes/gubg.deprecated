6167e3fdc39212d0abab8d1bae000609ac8831ea
Geert Fannes <geert.fannes@gmail.com>
2010-12-22 23:17:29 +0100

    Added generation of Bernoulli variables

--- ruby/gubg/math.rb
+++ ruby/gubg/math.rb
@@ -7,3 +7,24 @@ end
 def drawExponential(lambda)
 	-Math.log(rand)/lambda
 end
+
+def drawBernoulli(probSuccess)
+	rand <= probSuccess
+end
+
+def drawCategorical(probabilities, categories = nil)
+	prob = rand
+	cumulProb = 0
+	if categories
+		probabilities.zip(categories).each do |p, category|
+			cumulProb += p
+			return category if prob <= cumulProb
+		end
+	else
+		probabilities.each_with_index do |p, ix|
+			cumulProb += p
+			return ix if prob <= cumulProb
+		end
+	end
+	raise("Your probabilities do not add to 1 (#{probabilities}, #{prob})")
+end
