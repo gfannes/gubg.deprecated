abffb667984b9013c15dd5378463369d1f095979
Geert Fannes <geert.fannes@gmail.com>
2011-06-13 21:11:18 +0200

    Added SingletonThreadSafeUnlocker as an unlock strategy that uses one mutex per Data type

--- cpp/gubg/pointer/Locked.hpp
+++ cpp/gubg/pointer/Locked.hpp
@@ -10,6 +10,8 @@ namespace gubg
 {
     namespace pointer
     {
+        //Generic smart pointer that _only_ provides access to its pointer via an instance of Locked::Unlock
+        //Depending on the specific Unlocker used, this can implement different unlocking strategies
         template <typename Data, template <typename Data> class Unlocker>
             class Locked
             {
@@ -36,6 +38,8 @@ namespace gubg
                     DataPtr data_;
             };
 
+        //An unlock strategy that makes each instance of Data thread safe. Multiple instances can be access concurrently
+        // => Keeps one mutex per instance
         template <typename Data>
             class ThreadSafeUnlocker
             {
@@ -83,6 +87,58 @@ namespace gubg
                     typedef boost::scoped_ptr<Mutex::scoped_lock> LockPtr;
                     LockPtr lock_;
             };
+
+        //An unlock strategy that makes each type Data thread safe. Multiple instances of Data _cannot_ be accessed concurrently
+        // => Keeps one mutex per type
+        template <typename Data>
+            class SingletonThreadSafeUnlocker
+            {
+                public:
+                    typedef boost::recursive_mutex Mutex;
+                    typedef boost::condition Condition;
+                    struct LockingData
+                    {
+                        static Mutex mutex_;
+                        static Condition condition_;
+                    };
+                    typedef boost::shared_ptr<LockingData> LockingDataPtr;
+                    typedef boost::shared_ptr<Data> DataPtr;
+
+                    template <typename Locked>
+                    SingletonThreadSafeUnlocker(Locked locked):
+                        data_(locked.data_),
+                        lock_(new Mutex::scoped_lock(LockingData::mutex_))
+                    {
+                    }
+                    virtual ~SingletonThreadSafeUnlocker()
+                    {
+                        lock_.reset();
+                        LockingData::condition_.notify_all();
+                    }
+
+                    Data &operator*() const {return *data_;}
+                    Data *operator->() const {return data_.operator->();}
+                    Data *get() const {return data_.get();}
+                    bool operator()() const {return data_;}
+
+                    void wait()
+                    {
+                        LockingData::condition_.wait(*lock_);
+                    }
+                    bool timed_wait(boost::system_time timedOut)
+                    {
+                        return LockingData::condition_.timed_wait(*lock_, timedOut);
+                    }
+
+                private:
+                    DataPtr data_;
+                    typedef boost::scoped_ptr<Mutex::scoped_lock> LockPtr;
+                    LockPtr lock_;
+            };
+        template <typename Data>
+            boost::recursive_mutex SingletonThreadSafeUnlocker<Data>::LockingData::mutex_;
+        template <typename Data>
+            boost::condition SingletonThreadSafeUnlocker<Data>::LockingData::condition_;
     }
 }
 
--- cpp/gubg/pointer/test/test.cpp
+++ cpp/gubg/pointer/test/test.cpp
@@ -1,5 +1,8 @@
 #include "pointer/Locked.hpp"
 #include "testing/Testing.hpp"
+#include "threading/InstanceCounter.hpp"
+#include "sleep/sleep.hpp"
+#include "boost/thread.hpp"
 #define GUBG_LOG
 #include "logging/Log.hpp"
 using namespace gubg::pointer;
@@ -11,17 +14,38 @@ namespace
         public:
             typedef boost::shared_ptr<Data> Ptr;
 
-            Data():
-                value_(0)
-        {
-            LOG_SM(Data, "constructor");
-        }
+            Data()
+            {
+                LOG_SM(Data, "constructor");
+                value_ = 0;
+            }
             ~Data()
             {
                 LOG_SM(Data, "destructor, value_: " << value_);
             }
 
-            int value_;
+            static int value_;
+    };
+    int Data::value_ = 0;
+    typedef Locked<Data, ThreadSafeUnlocker> DataPtr;
+    typedef Locked<Data, SingletonThreadSafeUnlocker> SDataPtr;
+
+    template <typename DataPtr>
+    struct IncrementThread: gubg::InstanceCounter<IncrementThread<DataPtr>>
+    {
+        IncrementThread(DataPtr data):
+            data_(data),
+            thread_(boost::ref(*this)){}
+        void operator()()
+        {
+            typename DataPtr::Unlock unlock(data_);
+            int v = unlock->value_;
+            gubg::nanosleep(0, 100000000);
+            unlock->value_ = v+1;
+            delete this;
+        }
+        DataPtr data_;
+        boost::thread thread_;
     };
 }
 
@@ -29,7 +53,6 @@ int main()
 {
     TEST_TAG(main);
     LOG_S(main);
-    typedef Locked<Data, ThreadSafeUnlocker> DataPtr;
     {
         LOG_SM(constructor, "Constructor");
         {
@@ -53,6 +76,35 @@ int main()
             DataPtr::Unlock unlock2(data);
             ++unlock2->value_;
         }
+    }
+    {
+        LOG_SM(MultiThreaded, "MultiThreaded");
+        const int NrThreads = 10;
+        DataPtr data(new Data);
+        for (int i = 0; i < NrThreads; ++i)
+            new IncrementThread<DataPtr>(data);
+        while (IncrementThread<DataPtr>::nrInstances() > 0)
+            gubg::nanosleep(0, 100000000);
+        DataPtr::Unlock unlock(data);
+        TEST_EQ(NrThreads, unlock->value_);
+    }
+    {
+        LOG_SM(Singleton, "Mutex per instance");
+        DataPtr data1(new Data);
+        DataPtr data2(new Data);
+        new IncrementThread<DataPtr>(data1);
+        new IncrementThread<DataPtr>(data2);
+        while (IncrementThread<DataPtr>::nrInstances() > 0)
+            gubg::nanosleep(0, 100000000);
+    }
+    {
+        LOG_SM(Singleton, "Mutex per type");
+        SDataPtr data1(new Data);
+        SDataPtr data2(new Data);
+        new IncrementThread<SDataPtr>(data1);
+        new IncrementThread<SDataPtr>(data2);
+        while (IncrementThread<SDataPtr>::nrInstances() > 0)
+            gubg::nanosleep(0, 100000000);
     }
 
     return 0;
