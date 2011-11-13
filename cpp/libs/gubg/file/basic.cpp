5598ad476cbe7d1865e4de3bd1511e59ada66245
Geert Fannes <geert.fannes@gmail.com>
2011-11-13 17:53:19 +0100

    Switched to return-based MSS

--- cpp/libs/gubg/file/basic.cpp
+++ cpp/libs/gubg/file/basic.cpp
@@ -7,17 +7,16 @@ namespace
 {
     typedef gubg::file::basic::ReturnCode RC;
     template <typename Parts>
-    RC tokenizeAbsolutePath(Parts &parts, const string &path)
-    {
-        MSS_BEGIN(RC);
-        MSS_T(!path.empty(), PathIsEmpty);
-        parts = gubg::parse::tokenize(path, "/");
-        MSS_T(!parts.empty(), PartsIsEmpty);
-        MSS_T(parts[0] == "", PathIsNotAbsolute);
-        parts.pop_front();
-        MSS_FAIL();
-        MSS_RETURN();
-    }
+        RC tokenizeAbsolutePath(Parts &parts, const string &path)
+        {
+            MSS_BEGIN(RC);
+            MSS_T(!path.empty(), PathIsEmpty);
+            parts = gubg::parse::tokenize(path, "/");
+            MSS_T(!parts.empty(), PartsIsEmpty);
+            MSS_T(parts[0] == "", PathIsNotAbsolute);
+            parts.pop_front();
+            MSS_END();
+        }
 }
 
 gubg::file::basic::ReturnCode gubg::file::basic::expandPath(string &pathE, const string &path)
@@ -29,58 +28,53 @@ gubg::file::basic::ReturnCode gubg::file::basic::expandPath(string &pathE, const
     MSS_T(!parts.empty(), InvalidPath);
 
     //We construct the expanded path by gradually changing the working directory based on parts from path
+    deque<string> partsE;
+    MSS(tokenizeAbsolutePath(partsE, getCurrentWorkingDirectory()));
+    bool firstTime = true;
+    while (!parts.empty())
     {
-        deque<string> partsE;
-        MSS(tokenizeAbsolutePath(partsE, getCurrentWorkingDirectory()));
+        auto head = parts[0];
+        if (head == "")
         {
-            bool firstTime = true;
-            while (!parts.empty())
+            if (firstTime)
+                partsE.clear();
+            //We ignore "//" or trailing "/" that are not leading
+        }
+        else if (head == ".")
+        {
+            //Nothing to be done, we started from the current dir and "." means no change
+        }
+        else if (head == "..")
+        {
+            //Go up one dir, we don't fail if partsE is empty (alread at root)
+            if (!partsE.empty())
+                partsE.pop_back();
+        }
+        else if (head == "~")
+        {
+            if (firstTime)
             {
-                auto head = parts[0];
-                if (head == "")
-                {
-                    if (firstTime)
-                        partsE.clear();
-                    //We ignore "//" or trailing "/" that are not leading
-                }
-                else if (head == ".")
-                {
-                    //Nothing to be done, we started from the current dir and "." means no change
-                }
-                else if (head == "..")
-                {
-                    //Go up one dir, we don't fail if partsE is empty (alread at root)
-                    if (!partsE.empty())
-                        partsE.pop_back();
-                }
-                else if (head == "~")
-                {
-                    if (firstTime)
-                    {
-                        //We change to the $HOME directory
-                        MSS(tokenizeAbsolutePath(partsE, getHomeDirectory()));
-                    }
-                }
-                else
-                    partsE.push_back(head);
-
-                parts.pop_front();
-                firstTime = false;
+                //We change to the $HOME directory
+                MSS(tokenizeAbsolutePath(partsE, getHomeDirectory()));
             }
         }
+        else
+            partsE.push_back(head);
 
-        //Convert the parts into an absolute path
-        {
-            ostringstream oss;
-            if (partsE.empty())
-                oss << "/";
-            else
-                for (auto partE = partsE.begin(); partE != partsE.end(); ++partE)
-                    oss << "/" << *partE;
-            pathE = oss.str();
-        }
+        parts.pop_front();
+        firstTime = false;
     }
 
-    MSS_FAIL();
-    MSS_RETURN();
+    //Convert the parts into an absolute path
+    {
+        ostringstream oss;
+        if (partsE.empty())
+            oss << "/";
+        else
+            for (auto partE = partsE.begin(); partE != partsE.end(); ++partE)
+                oss << "/" << *partE;
+        pathE = oss.str();
+    }
+
+    MSS_END();
 }
