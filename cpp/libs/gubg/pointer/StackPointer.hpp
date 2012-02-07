7ed55c65e74a90ce1a2c69766cd43c4b2d169026
root <root@ri.localdomain>
2012-02-07 01:59:20 +0100

    Added statemachine.rb and got StackPointer working

--- cpp/libs/gubg/pointer/StackPointer.hpp
+++ cpp/libs/gubg/pointer/StackPointer.hpp
@@ -3,33 +3,36 @@
 
 namespace
 {
-    template <typename T>
-        class StackPointer
-        {
-            public:
-                StackPointer():
-                    valid(false){}
-                ~StackPointer()
-                {
-                    if (valid)
-                        reinterpret_cast<T*>(buffer)->~T();
-                }
-                template <typename... Args>
-                    T &operator()(Args... args)
-                    {
-                        if (!valid)
-                        {
-                            new (buffer) T(args...);
-                            valid = true;
-                        }
-                        return *reinterpret_cast<T*>(buffer);
-                    }
-            private:
-                //We put the buffer first, else it might be badly aligned
-                typedef unsigned char byte;
-                byte buffer[sizeof(T)];
-                bool valid;
-        };
+	template <typename T>
+		class StackPointer
+		{
+			public:
+				StackPointer():
+					valid(false){}
+				~StackPointer()
+				{
+					if (valid)
+						reinterpret_cast<T*>(buffer)->~T();
+				}
+				template <typename... Args>
+					bool create(Args&&... args)
+					{
+						if (valid)
+							return false;
+						new (buffer) T(std::forward<Args>(args)...);
+						valid = true;
+						return true;
+					}
+				template <typename... Args>
+					T &operator()(Args&&... args)
+					{
+						create(std::forward<Args>(args)...);
+						return *reinterpret_cast<T*>(buffer);
+					}
+			private:
+				size_t buffer[(sizeof(T)+sizeof(size_t)-1)/sizeof(size_t)];
+				bool valid;
+		};
 }
 
 #endif
--- vim/config.linux.vim
+++ vim/config.linux.vim
@@ -111,9 +111,9 @@ endif
 "Added by gfannes
 source $GUBG/vim/config.vim
 ""set lines=60 columns=190
-set lines=40 columns=120
+set lines=60 columns=120
 set backupdir=/tmp
 set directory=/tmp
 ""set gfn=Monospace\ 11
-set gfn=Monospace\ 20
+set gfn=Monospace\ 16
 set sel=exclusive
