335d569676b53cc1069808dece5b969c7e4f6b3b
Geert Fannes <geert.fannes@gmail.com>
2012-08-17 01:09:41 +0200

    Implemented a basic model for the OSI physical and datalink layers using crtp and policy-based design

---
+++ cpp/libs/gubg/osi/Datalink.hpp
@@ -0,0 +1,56 @@
+#ifndef gubg_osi_Datalink_hpp
+#define gubg_osi_Datalink_hpp
+
+#include "gubg/osi/Codes.hpp"
+#include "gubg/osi/Framer.hpp"
+#include <string>
+#include <list>
+
+namespace gubg
+{
+    namespace osi
+    {
+        template <typename Configuration>
+        class Datalink: public Framer_crtp<Datalink<Configuration>, Configuration>
+        {
+            public:
+                typedef typename Configuration::Frame Frame;
+
+                ReturnCode send(const Frame &frame)
+                {
+                    MSS_BEGIN(ReturnCode);
+                    outFrames_.push_back(frame);
+                    MSS_END();
+                }
+                ReturnCode receive(Frame &frame)
+                {
+                    MSS_BEGIN(ReturnCode);
+                    MSS(!inFrames_.empty());
+                    inFrames_.front().swap(frame);
+                    inFrames_.pop_front();
+                    MSS_END();
+                }
+
+                ReturnCode framer_receivedFrame(const Frame &frame)
+                {
+                    MSS_BEGIN(ReturnCode);
+                    inFrames_.push_back(frame);
+                    MSS_END();
+                }
+                ReturnCode framer_getNextFrameToSend(Frame &frame)
+                {
+                    MSS_BEGIN(ReturnCode);
+                    MSS(!outFrames_.empty());
+                    outFrames_.front().swap(frame);
+                    outFrames_.pop_front();
+                    MSS_END();
+                }
+            private:
+                typedef std::list<Frame> Frames;
+                Frames outFrames_;
+                Frames inFrames_;
+        };
+    }
+}
+
+#endif
