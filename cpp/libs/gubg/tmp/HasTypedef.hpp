dd54ddd3faacc98a06695108a0f367a568e5837b
Geert Fannes <geert.fannes@gmail.com>
2012-10-03 22:28:40 +0200

    Added some experimental HasTypedef tmp

---
+++ cpp/libs/gubg/tmp/HasTypedef.hpp
@@ -0,0 +1,34 @@
+#ifndef gubg_tmp_HasTypedef_hpp
+#define gubg_tmp_HasTypedef_hpp
+
+#include "gubg/tmp/Types.hpp"
+
+namespace gubg
+{
+    namespace tmp
+    {
+        template <typename T>
+            class HasTypedef
+            {
+                private:
+                    template <typename TT> static Small Test(typename TT::Name *);
+                    template <typename TT> static Large Test(...);
+                public:
+                    enum: bool {Value = (sizeof(Test<T>(0)) == sizeof(Small))};
+            };
+#if 0
+        template <typename T, typename U>
+            struct HasTypedef
+            {
+                enum: bool {Value = false};
+            };
+        template <typename T>
+            struct HasTypedef<T, typename T::Name>
+            {
+                enum: bool {Value = true};
+            };
+#endif
+    }
+}
+
+#endif
