3ed6e73c3fe9a08f2154a3b671529c129d30148b
Geert Fannes <geert.fannes@gmail.com>
2012-10-15 22:46:49 +0200

    Added SmartRange, a generic container-based range that keeps a shared_ptr ref to the underlying range

---
+++ cpp/libs/gubg/test/SmartRange_tests.cpp
@@ -0,0 +1,65 @@
+#include "gubg/testing/Testing.hpp"
+#include "gubg/SmartRange.hpp"
+#include "gubg/l.hpp"
+#include <string>
+using namespace gubg;
+using namespace std;
+
+namespace 
+{
+    typedef SmartRange<string> SR;
+}
+
+int main()
+{
+    TEST_TAG(SmartRange);
+
+    SR sr;
+    {
+        SR sr2("abc");
+        SR sr3(sr2);
+        for (auto ch: sr2)
+            L(ch);
+        sr = sr2;
+    }
+    for (auto ch: sr)
+        L(ch);
+
+    {
+        auto sp = sr;
+        TEST_EQ(3, sr.size());
+        TEST_FALSE(sr.empty());
+        ++sr.front();
+        ++sr[1];
+        ++sr.back();
+        sr.popFront();
+        TEST_EQ(2, sr.size());
+        sr.popFront();
+        sr.popFront();
+        TEST_TRUE(sr.empty());
+        sr = sp;
+    }
+
+    TEST_EQ(3, sr.size());
+    for (auto ch: sr)
+        L(ch);
+
+    {
+        auto p = sr.data();
+        p[2] = '\0';
+        L("p: " << p);
+
+        auto ssr = sr.subrange(1, 1);
+        ssr.front() = 'B';
+        L("p: " << p << "|" << ssr.data());
+    }
+
+    sr = "abc";
+    TEST_EQ(1, sr.find(sr.subrange(1, 1)));
+    TEST_EQ(1, sr.find("b"));
+    TEST_EQ(1, sr.find("b", 1));
+    TEST_EQ(SR::npos, sr.find("b", 2));
+    TEST_EQ(SR::npos, sr.find("B"));
+
+    return 0;
+}
