6dd39712b122fd5c1cf8ffd0b2921868b975f171
Geert Fannes <geert.fannes@gmail.com>
2012-11-26 23:44:56 +0100

    Trying to get heterogeneous graph up and running

--- cpp/libs/gubg/relation/Relation.hpp
+++ cpp/libs/gubg/relation/Relation.hpp
@@ -11,15 +11,15 @@ namespace gubg
         template <typename T>
             struct Many
             {
-                private:
+                protected:
                     typedef std::shared_ptr<T> Ptr_;
                     typedef std::vector<Ptr_> Ary;
 
-                    Ary ary;
+                    Ary ary_;
 
                 public:
-                    void addRelation(Ptr_ el){ary.push_back(el);}
-                    size_t nrRelations() const {return ary.size();}
+                    void addRelation(Ptr_ el){ary_.push_back(el);}
+                    size_t nrRelations() const {return ary_.size();}
             };
 
         template <typename From, typename To>
@@ -33,6 +33,45 @@ namespace gubg
             {
                 return from->Many<To>::nrRelations();
             }
+
+	template <typename From, typename To>
+		class Traits
+		{
+			private:
+				typedef char CanRelate_;
+				class CannotRelate_{char dummy[2];};
+				//The basic trick is to check which of the following overloads is chosen
+				//The Test(...) has lowest priority, the compiler will first try everything possible to convert MakeDerived() into const Base &
+				static CanRelate_ Test(const Many<To> &);
+				static CannotRelate_ Test(...);
+				static From &MakeFrom();
+			public:
+				static const bool CanRelate = (sizeof(Test(MakeFrom())) == sizeof(CanRelate_));
+		};
+
+    template <typename...Ts>
+        struct DFS_crtp;
+    template <typename Receiver, typename T, typename... Ts>
+        struct DFS_crtp<Receiver, T, Ts...>: Many<T>, DFS_crtp<Receiver, Ts...>
+        {
+            typedef Many<T> ManyBase;
+            typedef DFS_crtp<Receiver, Ts...> DFSBase;
+            void iterate()
+            {
+                for (auto ptr: ManyBase::ary_)
+                {
+                    receiver_().dfs_discover(*ptr);
+                }
+                DFSBase::iterate();
+            }
+            Receiver &receiver_(){return static_cast<Receiver&>(*this);}
+        };
+    template <typename Receiver>
+        struct DFS_crtp<Receiver>
+        {
+            void iterate(){}
+            Receiver &receiver_(){return static_cast<Receiver&>(*this);}
+        };
     }
 }
 
--- cpp/libs/gubg/relation/test/Relation_tests.cpp
+++ cpp/libs/gubg/relation/test/Relation_tests.cpp
@@ -1,6 +1,7 @@
 #include "gubg/testing/Testing.hpp"
 #define GUBG_LOG
 #include "gubg/relation/Relation.hpp"
+#include "gubg/l.hpp"
 using namespace gubg::relation;
 using namespace std;
 
@@ -20,6 +21,25 @@ struct C: Shareble<C>, Many<B>, Many<A>
 {
 };
 
+struct DFS: DFS_crtp<DFS, A>
+{
+    void dfs_discover(A &a)
+    {
+        L(&a);
+    }
+};
+struct DFS2: DFS_crtp<DFS2, A, B>
+{
+    void dfs_discover(A &a)
+    {
+        L("A::" << &a);
+    }
+    void dfs_discover(B &b)
+    {
+        L("B::" << &b);
+    }
+};
+
 int main()
 {
     TEST_TAG(main);
@@ -35,5 +55,19 @@ int main()
     TEST_EQ(1, c->Many<A>::nrRelations());
     TEST_EQ(3, c->Many<B>::nrRelations());
     TEST_EQ(3, nrRelations<B>(c));
+    TEST_TRUE((Traits<B, A>::CanRelate));
+    TEST_FALSE((Traits<A, B>::CanRelate));
+    TEST_TRUE((Traits<C, A>::CanRelate));
+    TEST_TRUE((Traits<C, B>::CanRelate));
+
+    DFS dfs;
+    dfs.addRelation(a);
+    dfs.addRelation(a);
+    dfs.iterate();
+
+    DFS2 dfs2;
+    dfs2.Many<A>::addRelation(a);
+    dfs2.Many<B>::addRelation(b);
+    dfs2.iterate();
     return 0;
 }
