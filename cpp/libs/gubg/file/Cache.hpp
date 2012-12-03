3668ebf53eaaecee227b6515ed5c9bc3ec2dec70
Geert Fannes <geert.fannes@gmail.com>
2012-12-03 08:32:21 +0100

    Added start of file cache

---
+++ cpp/libs/gubg/file/Cache.hpp
@@ -0,0 +1,17 @@
+#ifndef sdf
+#define sdf
+
+namespace gubg
+{
+    namespace file
+    {
+        template <typename Receiver>
+            class Cache_crtp
+            {
+                public:
+                private:
+            };
+    }
+}
+
+#endif
