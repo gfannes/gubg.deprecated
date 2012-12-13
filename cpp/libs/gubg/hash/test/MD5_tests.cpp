2efff8a43d4c43fcf421b9e6375e8e38f624cb54
Geert Fannes <geert.fannes@gmail.com>
2012-12-13 01:23:40 +0100

    Added a test vector for more that 1 block of data

--- cpp/libs/gubg/hash/test/MD5_tests.cpp
+++ cpp/libs/gubg/hash/test/MD5_tests.cpp
@@ -26,6 +26,15 @@ int main()
     TEST_TRUE(hash(md5, "The quick brown fox jumps over the lazy dog", "9e107d9d372bb6826bd81d3542a419d6"));
     TEST_TRUE(hash(md5, "The quick brown fox jumps over the lazy dog.", "e4d909c290d0fb1ca068ffaddf22cbd0"));
     TEST_TRUE(hash(md5, "aaa", "47bce5c74f589f4867dbd57e9ca9f808"));
+    TEST_TRUE(hash(md5, std::string(622, 'a')+'\xa', "ad09e2739607c40e36fb75450263276f"));
+
+    md5.clear();
+    md5 << "aaa";
+    auto h1 = md5.hash();
+    md5.clear();
+    md5 << "a" << "a" << "a";
+    auto h2 = md5.hash();
+    TEST_TRUE(h1 == h2);
         
     return 0;
 }
