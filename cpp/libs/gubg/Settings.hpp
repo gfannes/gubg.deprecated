690445a3607af5b0d7ed6fae8735c3e77d2963a5
Geert Fannes <geert.fannes@gmail.com>
2012-12-15 21:59:28 +0100

    Added add

--- cpp/libs/gubg/Settings.hpp
+++ cpp/libs/gubg/Settings.hpp
@@ -12,29 +12,73 @@ namespace gubg
         class Settings
         {
             public:
+                typedef std::string String;
+                typedef std::vector<String> Strings;
+
                 Settings():infoPerKey_(new InfoPerKey){}
 
-                bool set(Key key, const std::string &str)
+                bool set(Key key, const String &str)
                 {
                     auto it = infoPerKey_->find(key);
                     if (it != infoPerKey_->end())
                         //Key already exists
                         return false;
-                    (*infoPerKey_)[key] = createInfo_(str);
+                    (*infoPerKey_)[key] = createString_(str);
+                    return true;
+                }
+                bool add(Key key, const String &str)
+                {
+                    auto it = infoPerKey_->find(key);
+                    if (it == infoPerKey_->end())
+                    {
+                        (*infoPerKey_)[key] = createStrings_();
+                        it = infoPerKey_->find(key);
+                    }
+                    Info &info = *it->second;
+                    if (!info.is(Strings_))
+                        return false;
+                    info.strings_.push_back(str);
+                    return true;
+                }
+                bool get(String &str, Key key) const
+                {
+                    auto it = infoPerKey_->find(key);
+                    if (it == infoPerKey_->end())
+                        //Key does not exist
+                        return false;
+                    const Info &info = *it->second;
+                    if (!info.is(String_))
+                        return false;
+                    str = info.string_;
+                    return true;
+                }
+                bool get(Strings &strs, Key key) const
+                {
+                    auto it = infoPerKey_->find(key);
+                    if (it == infoPerKey_->end())
+                        //Key does not exist
+                        return false;
+                    const Info &info = *it->second;
+                    if (!info.is(Strings_))
+                        return false;
+                    strs = info.strings_;
                     return true;
                 }
 
             private:
-                enum Type {Internal, String, Strings};
+                enum Type {Internal, String_, Strings_};
                 struct Info
                 {
-                    Info(const std::string &str):type_(String), string_(str){}
+                    Info(const String &str):type_(String_), string_(str){}
+                    Info():type_(Strings_){}
+                    bool is(Type type) const {return type_ == type;}
                     Type type_;
                     Settings<Key> settings_;
-                    std::string string_;
-                    std::vector<std::string> strings_;
+                    String string_;
+                    Strings strings_;
                 };
-                static std::shared_ptr<Info> createInfo_(const std::string &str){return std::shared_ptr<Info>(new Info(str));}
+                static std::shared_ptr<Info> createString_(const String &str){return std::shared_ptr<Info>(new Info(str));}
+                static std::shared_ptr<Info> createStrings_(){return std::shared_ptr<Info>(new Info());}
                 typedef std::map<Key, std::shared_ptr<Info>> InfoPerKey;
                 std::shared_ptr<InfoPerKey> infoPerKey_;
         };
--- cpp/libs/gubg/test/Settings_tests.cpp
+++ cpp/libs/gubg/test/Settings_tests.cpp
@@ -1,5 +1,6 @@
 #include "gubg/Testing.hpp"
 #include "gubg/Settings.hpp"
+using namespace std;
 
 enum Key {Name, Includes, Options};
 
@@ -11,5 +12,14 @@ int main()
     Settings settings;
     TEST_TRUE(settings.set(Name, "Root"));
     TEST_FALSE(settings.set(Name, "Root"));
+    string str;
+    TEST_TRUE(settings.get(str, Name));
+    TEST_EQ("Root", str);
+    TEST_TRUE(settings.add(Includes, "aaa"));
+    TEST_TRUE(settings.add(Includes, "bbb"));
+    TEST_TRUE(settings.add(Includes, "ccc"));
+    vector<string> strs;
+    TEST_TRUE(settings.get(strs, Includes));
+    TEST_TRUE((strs == Settings::Strings({"aaa", "bbb", "ccc"})));
     return 0;
 }
