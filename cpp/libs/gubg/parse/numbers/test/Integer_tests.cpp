99876022b5751147c8b682326000beae3b1e7ad2
Geert Fannes <geert.fannes@gmail.com>
2013-04-20 00:14:08 +0200

    wip on reading numbers fast

--- cpp/libs/gubg/parse/numbers/test/Integer_tests.cpp
+++ cpp/libs/gubg/parse/numbers/test/Integer_tests.cpp
@@ -1,30 +1,58 @@
 #include "gubg/parse/numbers/Integer.hpp"
 #include "gubg/Testing.hpp"
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-#define GUBG_MODULE "test"
+#define GUBG_MODULE_ "test"
 #include "gubg/log/begin.hpp"
 int main()
 {
     TEST_TAG(main);
-    auto a = "1234567890";
-    auto aa = 1234567890;
-    unsigned long long bb = 0;
-    TEST_TRUE(gubg::parse::numbers::readInt(bb, a, 10));
-    TEST_EQ(aa, bb);
+    {
+        TEST_TAG(pos);
+        L(STREAM(sizeof(long), sizeof(long long)));
+        auto a = "1234567890bla";
+        long aa = 1234567890, bb = 0;
+        size_t l = 13;
+        TEST_TRUE(gubg::parse::numbers::read(bb, a, l));
+        TEST_EQ(aa, bb);
+        TEST_EQ(10, l);
+    }
     {
         TEST_TAG(stress);
-        auto Nr = 10000000;
+        const size_t Nr = 10000000;
+        vector<long>   ls(Nr);
+        iota(ls.begin(), ls.end(), 0);
+        vector<string> strs(Nr);
+        {
+            SSS("Generating numbers");
+            ostringstream oss;
+            transform(ls.begin(), ls.end(), strs.begin(), [&](long l){oss.str(""); oss << l; return oss.str();});
+        }
+
+        long bb;
+        size_t s;
+        long sum_mine = 0, sum_strtol = 0;
         {
             SSS("mine");
-            for (unsigned long i = 0; i < Nr; ++i)
-                gubg::parse::numbers::readInt(bb, a, 10);
+            for (size_t i = 0; i < Nr; ++i)
+            {
+                gubg::parse::numbers::read(bb, strs[i].c_str(), s = strs[i].size());
+                sum_mine += bb;
+            }
         }
         {
             SSS("strtol");
             char *e;
-            for (unsigned long i = 0; i < Nr; ++i)
-                bb = std::strtol(a, &e, 10);
+            for (size_t i = 0; i < Nr; ++i)
+            {
+                bb = std::strtol(strs[i].c_str(), &e, 10);
+                sum_strtol += bb;
+            }
         }
+        TEST_EQ(sum_mine, sum_strtol);
     }
     return 0;
 }
