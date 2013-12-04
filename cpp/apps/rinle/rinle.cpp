3d898bdae5e7b22e5c45c5700eaffc2a74fa04f5
Geert Fannes <geert.fannes@gmail.com>
2013-12-05 00:03:47 +0100

    some testing with nana

--- cpp/apps/rinle/rinle.cpp
+++ cpp/apps/rinle/rinle.cpp
@@ -1,15 +1,30 @@
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include "gubg/file/Filesystem.hpp"
+#include "gubg/parse/Line.hpp"
+#include <string>
+#include <vector>
+#include <algorithm>
+
+auto s = nana::rectangle(0, 0, 1365, 800);
+auto s2 = nana::rectangle(0, 200, 1365, 800);
 
 int main()
 {
     using namespace nana::gui;
 
-    form fm;
-    fm.caption(STR("Hello World"));
-    button btn(fm, nana::rectangle(20, 20, 150, 30));
-    btn.caption(STR("Quit"));
-    btn.make_event<events::click>(API::exit);
+    form fm(s);
+    fm.caption(STR("Rinle"));
+    std::string tmp;
+    gubg::file::read(tmp, gubg::file::File(__FILE__));
+    nana::string content(tmp.size(), '\0');
+    std::copy(tmp.begin(), tmp.end(), content.begin());
+    label lbl(fm, s);
+    lbl.caption(content);
+    lbl.make_event<events::click>(API::exit);
+    label lbl2(fm, s2);
+    lbl2.caption(content);
     fm.show();
     exec();
 } 
