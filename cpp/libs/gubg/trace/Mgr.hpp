ded6fe15202ecca29382dce94062179b31872671
Geert Fannes <geert.fannes@gmail.com>
2014-02-02 12:40:41 +0100

    wip

--- cpp/libs/gubg/trace/Mgr.hpp
+++ cpp/libs/gubg/trace/Mgr.hpp
@@ -2,12 +2,55 @@
 #define HEADER_gubg_trace_Mgr_hpp_ALREADY_INCLUDED
 
 #include "gubg/trace/Msg.hpp"
+#include "gubg/tree/Node.hpp"
 #include "gubg/Singleton.hpp"
 #include <thread>
+#include <memory>
+#include <map>
 
 #define GUBG_MODULE_ "trace::Mgr"
 #include "gubg/log/begin.hpp"
 namespace gubg { namespace trace { 
+    class Tree
+    {
+        public:
+            typedef std::shared_ptr<Tree> Ptr;
+            Tree(std::thread::id tid): tid_(tid) {}
+
+            void open(const Msg &msg)
+            {
+                if (!root_)
+                {
+                    root_ = Node::create();
+                    back_ = root_;
+                }
+                else
+                    back_ = back_.pushChild(Node::create());
+
+                back_.data().str = msg.str();
+                back_.data().category = msg.category();
+            }
+            //Returns false if the thread died
+            bool close()
+            {
+                back_ = back_.parent();
+                return back_;
+            }
+
+        private:
+            struct Data
+            {
+                std::string str;
+                int category = 0;
+            };
+            typedef gubg::tree::Node<Data> Node;
+
+            const std::thread::id tid_;
+
+            Node root_;
+            Node back_;
+            std::mutex mutex_;
+    };
     class Mgr
     {
         public:
@@ -24,10 +67,19 @@ namespace gubg { namespace trace {
             void open(const Msg &msg)
             {
                 S();L("Opening category " << msg.category() << " " << msg.str());
+                getTree_().open(msg);
             }
             void close()
             {
                 S();L("Closing");
+                if (!getTree_().close())
+                {
+                    //The thread died, we move this tree to the dead trees
+                    auto tree = cutTree_();
+                    assert(tree);
+                    LockGuard lg(mutex_);
+                    deadTrees_.push_back(tree);
+                }
             }
 
             void operator()()
@@ -45,6 +97,36 @@ namespace gubg { namespace trace {
                 quit_ = true;
                 thread_.join();
             }
+
+            Tree &getTree_()
+            {
+                LockGuard lg(mutex_);
+                const auto tid = std::this_thread::get_id();
+                Tree::Ptr &tree = treePerTid_[tid];
+                if (!tree)
+                    tree.reset(new Tree(tid));
+                return *tree;
+            }
+            Tree::Ptr cutTree_()
+            {
+                Tree::Ptr tree;
+                LockGuard lg(mutex_);
+                auto it = treePerTid_.find(std::this_thread::get_id());
+                if (it != treePerTid_.end())
+                {
+                    tree = it->second;
+                    treePerTid_.erase(it);
+                }
+                return tree;
+            }
+
+            typedef std::map<std::thread::id, Tree::Ptr> TreePerTid;
+            TreePerTid treePerTid_;
+            typedef std::vector<Tree::Ptr> DeadTrees;
+            DeadTrees deadTrees_;
+            typedef std::mutex Mutex;
+            typedef std::lock_guard<Mutex> LockGuard;
+            Mutex mutex_;
 
             volatile bool quit_ = false;
             std::thread thread_;
