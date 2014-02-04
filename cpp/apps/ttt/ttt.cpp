b515d48d43e9d9620ba9077a4b823cc975b54cde
Geert Fannes <geert.fannes@gmail.com>
2014-02-04 21:02:23 +0100

    Added a server socket and some basic select action

--- cpp/apps/ttt/ttt.cpp
+++ cpp/apps/ttt/ttt.cpp
@@ -8,6 +8,8 @@ using namespace std;
 using namespace gubg;
 using namespace ttt;
 
+#define GUBG_MODULE_ "ttt"
+#include "gubg/log/begin.hpp"
 namespace ttt { 
     void finalize(const string &msg, int ret)
     {
@@ -23,10 +25,34 @@ namespace ttt {
         os << "Port: " << options.port << endl;
         return os;
     }
+    class Select: public file::Select
+    {
+        public:
+            virtual void select_ready(file::Descriptor d, file::EventType et)
+            {
+                S();L(d << " is ready for some action: " << to_hr(et));
+                switch (et)
+                {
+                    case file::EventType::Open:
+                        {
+                            file::Descriptor c;
+                            d.accept(c);
+                            add(c, file::AccessMode::Read);
+                        }
+                        break;
+                    case file::EventType::Read:
+                        {
+                            std::string str(1024, '?');
+                            d.read(str);
+                            L(str);
+                        }
+                        break;
+                }
+            }
+        private:
+    };
 } 
 
-#define GUBG_MODULE_ "ttt"
-#include "gubg/log/begin.hpp"
 ReturnCode main_(int argc, char **argv)
 {
     MSS_BEGIN(ReturnCode);
@@ -39,6 +65,17 @@ ReturnCode main_(int argc, char **argv)
         parser.parse(args);
     }
     L(options);
+
+    auto d = file::Descriptor::listen(1234);
+    MSS(d.valid());
+    Select s;
+    s.add(d, file::AccessMode::Read);
+    while (true)
+    {
+        S();
+        s.process(std::chrono::seconds(60));
+    }
+
     MSS_END();
 }
 int main(int argc, char **argv)
