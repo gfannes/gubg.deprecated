18a46960df2bc71d4f9baf7d7b574ac18168ffee
Geert Fannes <geert.fannes@gmail.com>
2014-04-03 00:45:52 +0200

    More experiments

---
+++ rust/gubg/cartouche.rs
@@ -0,0 +1,5 @@
+
+pub fn cart()
+{
+    println!("cartouche")
+}
---
+++ rust/gubg/test/cartouche_tests.rs
@@ -0,0 +1,10 @@
+//#[path="../cartouche.rs"]
+//mod cartouche;
+//extern mod cartouche;
+extern mod gubg;
+
+fn main()
+{
+    println!("cartouche_tests");
+    gubg::cartouche::cart();
+}
