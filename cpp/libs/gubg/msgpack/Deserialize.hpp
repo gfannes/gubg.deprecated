8b3cd61a1cf3b4fa954b319dcb4b166ecb9560e7
Geert Fannes <geert.fannes@gmail.com>
2014-06-05 02:02:57 +0200

    added some comments

--- cpp/libs/gubg/msgpack/Deserialize.hpp
+++ cpp/libs/gubg/msgpack/Deserialize.hpp
@@ -4,23 +4,25 @@
 #include "gubg/msgpack/Types.hpp"
 #include "gubg/msgpack/Primitives.hpp"
 #include "gubg/array.hpp"
+#include <cassert>
 
 #define GUBG_MODULE_ "Deserialize"
 #include "gubg/log/begin.hpp"
-namespace gubg
-{
-    namespace msgpack
-    {
-        template <typename String>
-            class Wrapper
-            {
-                public:
-                    AttributeId aid;
-                    TypeId tid;
+namespace gubg { namespace msgpack {
 
-                    Wrapper() {}
-                    template <typename T>
-                        explicit Wrapper(T &t)
+    //Wraps a reference to an object and a few methods (msgpack_set()), type-erasure style.
+    //The non-virtual methods will call into those of the object passed during construction.
+    template <typename String>
+        class Wrapper
+        {
+            public:
+                AttributeId aid;
+                TypeId tid;
+
+                Wrapper() {}
+                //Type-erasure ctor, make sure t does not vanish
+                template <typename T>
+                    explicit Wrapper(T &t)
                     {
                         //Type erasure: we will copy obj byte for byte into data_, including its vtable
                         //Probably not very portable...
@@ -30,72 +32,80 @@ namespace gubg
                             *it = *ptr++;
                         dataIsValid_ = true;
                     }
-                    Wrapper(const Wrapper &rhs): aid(rhs.aid), tid(rhs.tid), data_(rhs.data_), dataIsValid_(rhs.dataIsValid_) {}
-                    Wrapper &operator=(const Wrapper &rhs)
-                    {
-                        aid = rhs.aid; tid = rhs.tid; data_ = rhs.data_; dataIsValid_ = rhs.dataIsValid_;
-                        return *this;
-                    }
+                Wrapper(const Wrapper &rhs): aid(rhs.aid), tid(rhs.tid), data_(rhs.data_), dataIsValid_(rhs.dataIsValid_) {}
+                Wrapper &operator=(const Wrapper &rhs)
+                {
+                    aid = rhs.aid; tid = rhs.tid; data_ = rhs.data_; dataIsValid_ = rhs.dataIsValid_;
+                    return *this;
+                }
 
-                    bool isValid() const {return itf_() != 0;}
+                bool isValid() const {return itf_() != 0;}
 
-                    void set(AttributeId aid, Nil_tag nil)       {itf_()->set(aid, nil);}
-                    void set(AttributeId aid, long v)            {itf_()->set(aid, v);}
-                    void set(AttributeId aid, const String &str) {itf_()->set(aid, str);}
+                //Calls into the corresponding msgpack_set() methods of the object passed during ctor.
+                void set(AttributeId aid, Nil_tag nil)       {assert(isValid()); itf_()->set(aid, nil);}
+                void set(AttributeId aid, long v)            {assert(isValid()); itf_()->set(aid, v);}
+                void set(AttributeId aid, const String &str) {assert(isValid()); itf_()->set(aid, str);}
 
-                private:
-                    class Object_itf
-                    {
-                        public:
-                            Object_itf(): ptr_(0) {}
-                            Object_itf(void *ptr): ptr_(ptr) {}
+            private:
+                //Unity-sausage interface
+                class Object_itf
+                {
+                    public:
+                        Object_itf(): ptr_(0) {}
+                        Object_itf(void *ptr): ptr_(ptr) {}
 
-                            virtual void set(AttributeId, Nil_tag) {S();L("nil");}
-                            virtual void set(AttributeId, long v) {S();L(v);}
-                            virtual void set(AttributeId, const String &str) {S();L(str);}
+                        virtual void set(AttributeId, Nil_tag) {S();L("nil");}
+                        virtual void set(AttributeId, long v) {S();L(v);}
+                        virtual void set(AttributeId, const String &str) {S();L(str);}
 
-                        protected:
-                            //We will store the address of the wrapped object here
-                            void *ptr_;
-                    };
+                    protected:
+                        //We will store the address of the wrapped object here
+                        void *ptr_;
+                };
 
-                    typedef gubg::array<uint8_t, sizeof(Object_itf)> Data;
-                    Data data_;
-                    bool dataIsValid_ = false;
-                    Object_itf *itf_() {return dataIsValid_ ? (Object_itf*)data_.data() : 0;}
-                    const Object_itf *itf_() const {return dataIsValid_ ? (const Object_itf*)data_.data() : 0;}
+                //We will store a _binary_ copy of an Object instance into data_, with vtable and all
+                typedef gubg::array<uint8_t, sizeof(Object_itf)> Data;
+                Data data_;
+                bool dataIsValid_ = false;
 
-                    template <typename T>
-                        class Object: public Object_itf
-                    {
-                        private:
-                            typedef Object_itf Base;
+                Object_itf *itf_() {return dataIsValid_ ? (Object_itf*)data_.data() : 0;}
+                const Object_itf *itf_() const {return dataIsValid_ ? (const Object_itf*)data_.data() : 0;}
 
-                        public:
-                            Object(T &t): Base(&t) {}
-                            virtual void set(AttributeId aid, Nil_tag nil)       { obj_().msgpack_set(aid, nil); }
-                            virtual void set(AttributeId aid, long v)            { obj_().msgpack_set(aid, v); }
-                            virtual void set(AttributeId aid, const String &str) { obj_().msgpack_set(aid, str); }
+                template <typename T>
+                    class Object: public Object_itf
+                {
+                    private:
+                        typedef Object_itf Base;
 
-                        private:
-                            T &obj_() {return *reinterpret_cast<T*>(Base::ptr_);}
-                    };
-            };
+                    public:
+                        Object(T &t): Base(&t) {}
+                        virtual void set(AttributeId aid, Nil_tag nil)       { obj_().msgpack_set(aid, nil); }
+                        virtual void set(AttributeId aid, long v)            { obj_().msgpack_set(aid, v); }
+                        virtual void set(AttributeId aid, const String &str) { obj_().msgpack_set(aid, str); }
 
-        template <typename String, typename T>
-            Wrapper<String> wrap(T &t)
-            {
-                //We make sure to clear t before deserializing it
-                t = T();
-                return Wrapper<String>(t);
-            }
-        template <typename String, typename T>
-            Wrapper<String> wrapWithoutClear(T &t)
-            {
-                return Wrapper<String>(t);
-            }
-    }
-}
+                    private:
+                        //Reversal of the type erasure
+                        T &obj_() {return *reinterpret_cast<T*>(Base::ptr_);}
+                };
+        };
+
+    //Helper funcions to wrap an object. Typically, you want to clear the object first,
+    //reducing any dependency an previous garbage in it.
+    template <typename String, typename T>
+        Wrapper<String> wrap(T &t)
+        {
+            //We make sure to clear t before deserializing it
+            t = T();
+            return Wrapper<String>(t);
+        }
+    //If you know what you are doing, you can skip clearing the object
+    template <typename String, typename T>
+        Wrapper<String> wrapWithoutClear(T &t)
+        {
+            return Wrapper<String>(t);
+        }
+
+} }
 #include "gubg/log/end.hpp"
 
 #endif
