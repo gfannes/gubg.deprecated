afe2bdd086b3634940e7b5183e420f0a6040379f
Geert Fannes <geert.fannes@gmail.com>
2014-10-05 19:07:03 +0200

    added dtor and reset and operator=

--- cpp/libs/gubg/enclosed_ptr.hpp
+++ cpp/libs/gubg/enclosed_ptr.hpp
@@ -7,20 +7,25 @@ namespace gubg {
         class enclosed_ptr
         {
             public:
-                enclosed_ptr();
+                enclosed_ptr() = default;
                 template <typename TT, size_t SS>
                     enclosed_ptr(const enclosed_ptr<TT, SS> &);
+                template <typename TT, size_t SS>
+                    enclosed_ptr &operator=(const enclosed_ptr<TT, SS> &);
+                ~enclosed_ptr();
 
                 T *get() throw();
 
                 template <typename D>
                     T *create_new() throw();
 
+                void reset();
+
             private:
                 template <typename TT, size_t SS>
                     friend class enclosed_ptr;
                 typedef void (*cctor_t)(void*, const void*);
-                cctor_t cctor_ = 0;
+                cctor_t cctor_ = nullptr;
                 std::uint8_t data_[Size];
                 size_t size_;
         };
@@ -29,6 +34,7 @@ namespace gubg {
         template <typename T>
             void cctor_(void *dst, const void *src)
             {
+                //Placement-new copy constructor
                 new ((void*)dst) T(*(const T*)src);
             }
     } 
@@ -37,8 +43,17 @@ namespace gubg {
 #define L_TEMPLATE template <typename T, size_t Size>
 #define L_TYPE enclosed_ptr<T, Size>
     L_TEMPLATE
-        L_TYPE::enclosed_ptr()
+        L_TYPE::~enclosed_ptr()
         {
+            if (auto ptr = get())
+                ptr->~T();
+        }
+    L_TEMPLATE
+        void L_TYPE::reset()
+        {
+            if (auto ptr = get())
+                ptr->~T();
+            cctor_ = 0;
         }
     L_TEMPLATE
         template <typename TT, size_t SS>
@@ -51,6 +66,27 @@ namespace gubg {
             ep.cctor_(data_, ep.data_);
             size_ = ep.size_;
             cctor_ = ep.cctor_;
+        }
+    L_TEMPLATE
+        template <typename TT, size_t SS>
+        L_TYPE &L_TYPE::operator=(const enclosed_ptr<TT, SS> &rhs)
+        {
+            if (!rhs.cctor_)
+                //rhs is invalid, we becomes invalid too
+                reset();
+            else if (Size < rhs.size_)
+                //rhs is valid, but we have not enough place to store it
+                reset();
+            else
+            {
+                if (data_ != rhs.data_)
+                {
+                    rhs.cctor_(data_, rhs.data_);
+                    size_ = rhs.size_;
+                    cctor_ = rhs.cctor_;
+                }
+            }
+            return *this;
         }
     L_TEMPLATE
         T *L_TYPE::get() throw()
@@ -66,6 +102,7 @@ namespace gubg {
             if (Size < sizeof(D))
                 return 0;
             D *ptr = (D*)data_;
+            //Placement-new constructor
             new ((void*)ptr) D();
             cctor_ = details::cctor_<D>;
             size_ = sizeof(D);
--- cpp/libs/gubg/test/enclosed_ptr_tests.cpp
+++ cpp/libs/gubg/test/enclosed_ptr_tests.cpp
@@ -1,10 +1,16 @@
 #include "catch/catch.hpp"
 #include "gubg/enclosed_ptr.hpp"
 
+#define GUBG_MODULE_ "test"
+#include "gubg/log/begin.hpp"
 namespace  { 
     class B
     {
         public:
+            B(){S();L(this << " ctor " << name_);}
+            B(const B &rhs){S();L(this << " cctor");}
+            virtual ~B(){S();L(this << " dtor");}
+
             std::string name_ = "B";
 
             virtual int answer() const {return 0;}
@@ -16,16 +22,22 @@ namespace  {
 } 
 TEST_CASE("same type", "[enclosed_ptr]")
 {
+    S();
     BPtr ptr;
     SECTION("default ctor should return nullptr") { REQUIRE(nullptr == ptr.get()); }
     SECTION("different sizes")
     {
+        S();L("different sizes");
         typedef gubg::enclosed_ptr<B, sizeof(B)> SPtr;
         SPtr sptr; sptr.create_new<B>();
+        std::cout << sptr.get()->name_ << std::endl;
         REQUIRE(!!sptr.get());
+        L("before");
         ptr = sptr;
+        L("after");
+        std::cout << ptr.get()->name_ << std::endl;
         REQUIRE(!!ptr.get());
-        REQUIRE(ptr.get()->name_ == "B");
+        REQUIRE((ptr.get()->name_ == "B"));
     }
 }
 
@@ -33,7 +45,13 @@ namespace  {
     class D: public B
     {
         public:
-            D() {name_ = "D";}
+            D()
+            {
+                S();L(this << " ctor");
+                name_ = "D";
+            }
+            D(const D &rhs){S();L(this << " cctor");}
+            virtual ~D(){S();L(this << " dtor");}
 
             int answer() const override {return 42;}
 
@@ -44,9 +62,11 @@ namespace  {
 } 
 TEST_CASE("inherited types", "[enclosed_ptr]")
 {
+    S();
     typedef gubg::enclosed_ptr<D, sizeof(D)> DPtr;
     SECTION("create_new for B and D")
     {
+        S();
         BPtr bptr;
         REQUIRE(bptr.create_new<B>());
         REQUIRE(bptr.get()->name_ == "B");
@@ -55,8 +75,10 @@ TEST_CASE("inherited types", "[enclosed_ptr]")
     }
     SECTION("")
     {
+        S();
         DPtr dptr; dptr.create_new<D>();
         BPtr bptr(dptr);
         REQUIRE(42 == bptr.get()->answer());
     }
 }
+#include "gubg/log/end.hpp"
